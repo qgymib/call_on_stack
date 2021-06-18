274aae43e2cb2131da2086311042e1b43d3d63e3
qgymib <qgymib@gmail.com>
2021-06-18 16:10:49 +0800

    use less stack

--- src/call_on_stack.c
+++ src/call_on_stack.c
@@ -6,38 +6,41 @@
 #define ALIGN_SIZE(size, align)	\
 	(((uintptr_t)(size) + ((uintptr_t)(align) - 1)) & ~((uintptr_t)(align) - 1))
 
+#define STACK_ALIGN_SIZE (sizeof(void*) * 2)
+
 /**
  * @brief Call `func(arg)` in stack and jump back to `orig`
  * @param[in] orig      Jumpback address
  * @param[in] longjmp   Jumpback function
+ * @param[in] baseaddr  Stack base addr
  * @param[in] func      User defined function
  * @param[in] arg       User defined argument
  */
 void call_on_stack_asm(jmp_buf orig, void (*longjmp)(jmp_buf, int),
-    void (*func)(void* arg), void* arg);
+    void* baseaddr, void (*func)(void* arg), void* arg);
 
 void call_on_stack(void* stack, size_t size, void (*func)(void* arg), void* arg)
 {
-    assert(size > sizeof(jmp_buf) + sizeof(void*));
+    assert(size > STACK_ALIGN_SIZE * 2);
 
-    /**
-     * Calculate stack base address
-     * The stack address must align to sizeof(void*)
-     */
-    void* base_addr = (char*)stack + size;
-    void* align_addr = (void*)ALIGN_SIZE(base_addr, sizeof(void*));
-    if (base_addr != align_addr)
-    {
-        base_addr = align_addr - sizeof(void*);
-    }
-
-    /* Save context */
-    void* jump_addr = base_addr - sizeof(jmp_buf);
-    if (setjmp(jump_addr) != 0)
+    /* Save registers */
+    jmp_buf buf;
+    if (setjmp(buf) != 0)
     {
         return;
     }
 
+    /**
+     * Calculate stack base address
+     * The stack address must align to ensure meet PCS requirements
+     */
+    void* base_addr = (char*)stack + size;
+    void* align_addr = (void*)ALIGN_SIZE(base_addr, STACK_ALIGN_SIZE);
+    if (base_addr != align_addr)
+    {
+        base_addr = align_addr - STACK_ALIGN_SIZE;
+    }
+
     /* Switch stack and call function */
-    call_on_stack_asm(jump_addr, longjmp, func, arg);
+    call_on_stack_asm(buf, longjmp, base_addr, func, arg);
 }
